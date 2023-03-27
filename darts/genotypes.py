373ca196a74a3a0fad2f89fe0368487b6ced0516
xh-captain <73862652+xh-captain@users.noreply.github.com>
2023-03-27 13:17:07 +0800

    Update genotypes.py

--- darts/genotypes.py
+++ darts/genotypes.py
@@ -12,8 +12,6 @@ PRIMITIVES = [
     'Mcs_sepConv_3x3',
     'Mcs_sepConv_5x5',
     'Mcs_sepConv_7x7',
-    # 'dil_conv_3x3',
-    # 'dil_conv_5x5'
 ]
 #---houston
 HSI=Genotype(normal=[('max_pool_3x3', 0), ('avg_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 2), ('max_pool_3x3', 0), ('avg_pool_3x3', 2), ('Mcs_sepConv_3x3', 0), ('skip_connect', 4)], normal_concat=range(2, 6), reduce=[('Mcs_sepConv_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 0), ('Mcs_sepConv_3x3', 1), ('max_pool_3x3', 0), ('avg_pool_3x3', 3), ('Mcs_sepConv_5x5', 1), ('max_pool_3x3', 2)], reduce_concat=range(2, 6))
